aacc8fbb15ca9e5023d583893afc6b2a4302a801
tvillari <tvillari@leafnoise.io>
2024-03-27 17:37:59 -0300

    Entregable 2, Villari

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'clase-04';
+  alumnos = ['alumno1', 'alumno2', 'alumno3', 'alumno4'];
+  listaDeAlumnos = true;
+  lista = true;
+  // hasError = false;
+  // isDisable = true;
+  // students = ['uno', 'dos', 'tres'];
+
+  // status: 'en-camino' | 'entregado' | 'en-preparacion' = 'en-preparacion';
+
+  // alternalError() {
+  //   this.hasError = !this.hasError;
+  // }
 }
